e168e65357b0d8866a7a384a2d183ee74ccbb0fa
vaib-deloitte <vkrishana@deloitte.com>
2022-05-23 16:12:59 +0530

    Delete Q1.py

--- Q1.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-numbers = [[1,1,3,2],[9,8,8,1],[0,4,5,0,0,1,4]]
-
-counts = dict(Counter(numbers))
-duplicates = {key:value for key, value in counts.items() if value > 1}
-print(duplicates)
